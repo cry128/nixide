3cdfae01b950c60b951e98a10ca501123a5122ff
_cry64 <them@dobutterfliescry.net>
2026-03-19 03:53:01 +1000

    fix EvalStateBuilderExt

--- nixide/src/flake/eval_state_builder_ext.rs
+++ nixide/src/flake/eval_state_builder_ext.rs
@@ -1,17 +1,14 @@
+use super::FlakeSettings;
+use crate::{EvalStateBuilder, NixErrorCode};
+
 pub trait EvalStateBuilderExt {
     /// Configures the eval state to provide flakes features such as `builtins.getFlake`.
-    fn flakes(
-        self,
-        settings: &FlakeSettings,
-    ) -> Result<nix_bindings_expr::eval_state::EvalStateBuilder>;
+    fn flakes(self, settings: &FlakeSettings) -> Result<EvalStateBuilder, NixErrorCode>;
 }
-impl EvalStateBuilderExt for nix_bindings_expr::eval_state::EvalStateBuilder {
+
+impl EvalStateBuilderExt for EvalStateBuilder {
     /// Configures the eval state to provide flakes features such as `builtins.getFlake`.
-    fn flakes(
-        mut self,
-        settings: &FlakeSettings,
-    ) -> Result<nix_bindings_expr::eval_state::EvalStateBuilder> {
-        settings.add_to_eval_state_builder(&mut self)?;
-        Ok(self)
+    fn flakes(mut self, settings: &FlakeSettings) -> Result<EvalStateBuilder, NixErrorCode> {
+        settings.add_to_eval_state_builder(&mut self).map(|_| self)
     }
 }
