70ce1b2b9e6e1da89a11f1c2d5e160addacf1836
_cry64 <them@dobutterfliescry.net>
2026-04-02 09:52:38 +1000

    fix bad flake clang config

--- flake.nix
+++ flake.nix
@@ -155,32 +155,6 @@
               nixForBindings
             ];
 
-            # bindgen uses clang to generate bindings, but it doesn't know where to
-            # find our stdenv cc's headers, so when it's gcc, we need to tell it.
-            postConfigure = lib.optionalString pkgs.stdenv.cc.isGNU ''
-              #!/usr/bin/env bash
-              # REF: https://github.com/nixops4/nix-bindings-rust/blob/main/bindgen-gcc.sh
-              # Rust bindgen uses Clang to generate bindings, but that means that it can't
-              # find the "system" or compiler headers when the stdenv compiler is GCC.
-              # This script tells it where to find them.
-
-              echo "Extending BINDGEN_EXTRA_CLANG_ARGS with system include paths..." 2>&1
-              BINDGEN_EXTRA_CLANG_ARGS="$${BINDGEN_EXTRA_CLANG_ARGS:-}"
-              export BINDGEN_EXTRA_CLANG_ARGS
-              include_paths=$(
-                echo | $NIX_CC_UNWRAPPED -v -E -x c - 2>&1 \
-                | awk '/#include <...> search starts here:/{flag=1;next} \
-                      /End of search list./{flag=0} \
-                      flag==1 {print $1}'
-              )
-              for path in $include_paths; do
-                echo " - $path" 2>&1
-                BINDGEN_EXTRA_CLANG_ARGS="$BINDGEN_EXTRA_CLANG_ARGS -I$path"
-              done
-            '';
-
-            shellHook = postConfigure;
-
             env = let
               inherit (llvmPackages) llvm libclang;
             in {
